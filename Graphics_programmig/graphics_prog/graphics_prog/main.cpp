26fb8a8e76193082b0197cf0e0c432577443a56f
Yuki Kawano <36154942+yukijuki@users.noreply.github.com>
2020-05-05 12:46:06 +0900

    Add files via upload

---
+++ Graphics_programmig/graphics_prog/graphics_prog/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  graphics_prog
+//
+//  Created by YukiKawano on 2020/05/05.
+//  Copyright © 2020 YukiKawano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    i = 10;
+    
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, SFC!\n";
+    std::cout << i << "\n";
+    
+    for (int i =0; i > 5; i++){
+        
+    }
+    
+    return 0;
+}
